77143a05876bb3343c89c0815c9b81280f7338c3
Kevin Etienne <kevin@incuna.com>
2016-09-14 21:04:20 +0100

    Set custom file

--- emacs/init.el
+++ emacs/init.el
@@ -166,17 +166,5 @@
 
 (add-hook 'clojure-mode-hook #'my-clojure-mode-hook)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (fill-column-indicator virtualenvwrapper rainbow-delimiters linum-relative jedi flycheck exec-path-from-shell evil-visualstar evil-leader))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file)
